85dbf8fa5f818c21a8808db5e2a3cdffe22715cf
wian0105 <wian.sanjaya0801@gmail.com>
2024-03-19 11:32:56 +0700

    save data di form dan tampilin data ke table

--- main.js
+++ main.js
@@ -2,15 +2,61 @@ var data = [];
 
 function addData(dataBaru) {
     data.push(dataBaru);
+    saveData();
 }
 
 function displayData() {
     var tableBody = document.getElementById('dataBody');
     tableBody.innerHTML = '';
     for (var item of data) {
-        var baris = tableBody.insertRow();
-        var cell = row.insertCell();
-        cell.innerText = item.id;
-        var name
+        var row = tableBody.insertRow();
+        var nimCell = row.insertCell();
+        nimCell.innerText = item.nim;
+        var namaCell = row.insertCell();
+        namaCell.innerText = item.nama;
+        var alamatCell = row.insertCell();
+        alamatCell.innerText = item.alamat;
     }
 }
+
+function saveData() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/save-data', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            console.log(xhr.responseText);
+        } else {
+            console.error();
+        }
+    };
+    xhr.send(JSON.stringify(data));
+}
+
+document.getElementById('dataForm').addEventListener('submit', function () {
+    event.preventDefault();
+    var nim = document.getElementById('nim').value;
+    var nama = document.getElementById('nama').value;
+    var alamat = document.getElementById('alamat').value;
+    if (!nim ||!nama ||!alamat) {
+        alert('Wajib mengisi form');
+        return;
+    }
+    addData({nim: parseInt(nim), nama: nama, alamat: alamat});
+    displayData();
+    document.getElementById('nim').value = '';
+    document.getElementById('nama').value = '';
+    document.getElementById('alamat').value = '';
+});
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', dataFile, true);
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        data = JSON.parse(xhr.responseText);
+        displayData();
+    } else {
+        console.error(xhr.responseText);
+    }
+};
+xhr.send();
